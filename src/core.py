28f80d43150eef1650723be24b9612b78785baa1
Kurage-237 <tukan222203@gmail.com>
2025-05-20 13:20:58 +0300

    done

---
+++ src/core.py
@@ -0,0 +1,28 @@
+class Category:
+    name: str
+    description: str
+    products: list
+
+    category_count = 0
+    products_count = 0
+
+    def __init__(self, name, description, products):
+        self.name = name
+        self.description = description
+        self.products = products
+
+        Category.category_count += 1
+
+
+class Product:
+    name: str
+    description: str
+    price: float
+    quantity: int
+
+    def __init__(self, name, description, price, quantity):
+        self.name = name
+        self.description = description
+        self.price = price
+        self.quantity = quantity
+        Category.products_count += 1
---
+++ tests/test_core.py
@@ -0,0 +1,34 @@
+from src import core
+import pytest
+import random
+
+@pytest.fixture
+def init_n_categories() -> (list, int):
+    n = random.randint(10,20)
+    objects = []
+    for x in range(n):
+        objects.append(core.Category("test", "test", []))
+    return objects, n
+
+def test_init_category(init_n_categories) -> None:
+    assert core.Category.category_count == init_n_categories[1]
+    for x in init_n_categories[0]:
+        assert x.name == "test"
+        assert x.description == "test"
+        assert x.products == []
+
+@pytest.fixture
+def init_n_products() -> (list, int):
+    n = random.randint(10,20)
+    objects = []
+    for x in range(n):
+        objects.append(core.Product("test", "test", float(n), n))
+    return objects, n
+
+def test_init_product(init_n_products) -> None:
+    assert core.Category.products_count == init_n_products[1]
+    for x in init_n_products[0]:
+        assert x.name == "test"
+        assert x.description == "test"
+        assert x.price == init_n_products[1]
+        assert x.quantity == init_n_products[1]
